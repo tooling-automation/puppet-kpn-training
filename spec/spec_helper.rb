81154e94cba535b54b0447d7eb530645bb7d6a84
rl504688 <roel.leijendeckers@kpn.com>
2025-01-31 11:57:58 +0100

    spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,41 +6,6 @@ require 'rspec-puppet-facts'
 include RspecPuppetFacts
 # rubocop:enable Style/MixinUsage
 
-add_custom_fact :customer_name, 'kpn'
-add_custom_fact :customer_environment, 'development'
-add_custom_fact :defaultgatewayinterface_ip, '12.12.12.12'
-add_custom_fact :choco_install_path, 'C:\Management\Programs\Chocolatey'
-add_custom_fact :chocolateyversion, '0.10.8'
-
-add_custom_fact :os,
-                family: 'windows',
-                architecture: 'x64',
-                hardware: 'x86_64',
-                name: 'windows',
-                windows: { system32: 'C:\windows\system32' },
-                release: { major: '2012 R2', full: '2012 R2' },
-                confine: 'windows-2012 R2-x64'
-
-add_custom_fact :os,
-                family: 'windows',
-                architecture: 'x64',
-                hardware: 'x86_64',
-                name: 'windows',
-                windows: { system32: 'C:\windows\system32' },
-                release: { major: '2016', full: '2016' },
-                confine: 'windows-2016-x64'
-
-add_custom_fact :os,
-                family: 'windows',
-                architecture: 'x64',
-                hardware: 'x86_64',
-                name: 'windows',
-                windows: { system32: 'C:\windows\system32' },
-                release: { major: '2019', full: '2019' },
-                confine: 'windows-2019-x64'
-
-add_custom_fact :kernel, 'windows'
-
 RSpec.configure do |c|
   c.fail_fast = true
   c.hiera_config = File.expand_path(File.join(__FILE__, '../hiera/rspec.yaml'))
